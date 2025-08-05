8751610e160b5341cf76e10bb6d9c9ca8548e452
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2025-08-04 23:42:12 -0300

    config inicial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+    import '../global.css'
+    import '@picocss/pico/css/pico.css';
+
+	let { children } = $props();
+</script>
+
+<style>
+    main {
+        padding-top: 3rem;
+    }
+</style>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+  
+<main class="container">
+	{@render children?.()}
+</main>
+
